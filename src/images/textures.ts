0614bd93151cb56376ea8be9d4ce034e6d632b19
james <jamesdockal@gmail.com>
2022-10-08 18:18:50 -0300

    feat: add basic camera position

--- src/images/textures.ts
+++ src/images/textures.ts
@@ -1,19 +1,13 @@
-import { NearestFilter, TextureLoader, RepeatWrapping } from 'three'
+import { NearestFilter, TextureLoader, RepeatWrapping } from "three";
 
-import {
-	dirtImg,
-	logImg,
-	grassImg,
-	glassImg,
-	woodImg
-} from './images'
+import { dirtImg, logImg, grassImg, glassImg, woodImg } from "./images";
 
-const dirtTexture = new TextureLoader().load(dirtImg)
-const logTexture = new TextureLoader().load(logImg)
-const grassTexture = new TextureLoader().load(grassImg)
-const glassTexture = new TextureLoader().load(glassImg)
-const woodTexture = new TextureLoader().load(woodImg)
-const groundTexture = new TextureLoader().load(grassImg)
+const dirtTexture = new TextureLoader().load(dirtImg);
+const logTexture = new TextureLoader().load(logImg);
+const grassTexture = new TextureLoader().load(grassImg);
+const glassTexture = new TextureLoader().load(glassImg);
+const woodTexture = new TextureLoader().load(woodImg);
+const groundTexture = new TextureLoader().load(grassImg);
 
 dirtTexture.magFilter = NearestFilter;
 logTexture.magFilter = NearestFilter;
@@ -21,14 +15,14 @@ grassTexture.magFilter = NearestFilter;
 glassTexture.magFilter = NearestFilter;
 woodTexture.magFilter = NearestFilter;
 groundTexture.magFilter = NearestFilter;
-groundTexture.wrapS = RepeatWrapping
-groundTexture.wrapT = RepeatWrapping
+groundTexture.wrapS = RepeatWrapping;
+groundTexture.wrapT = RepeatWrapping;
 
 export {
-	dirtTexture,
-	logTexture,
-	grassTexture,
-	glassTexture,
-	woodTexture,
-	groundTexture
-}
+  dirtTexture,
+  logTexture,
+  grassTexture,
+  glassTexture,
+  woodTexture,
+  groundTexture,
+};

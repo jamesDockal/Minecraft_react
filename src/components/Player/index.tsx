c505ef9e0c6db12212a0ac9e8f2d12e92b9cc7fb
james <jamesdockal@gmail.com>
2022-10-09 17:49:35 -0300

    feat: add keyboard listen and Player movimentation

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -2,8 +2,12 @@ import { useSphere } from "@react-three/cannon";
 import { useFrame, useThree } from "@react-three/fiber";
 import React, { useEffect, useRef } from "react";
 import { Vector3 } from "three";
+import { useKeyboard } from "../../hooks/useKeyboard";
 
 export const Player: React.FC = () => {
+  const { jump, moveBackward, moveForward, moveLeft, moveRight } =
+    useKeyboard();
+  console.log("moveRight", moveRight);
   const { camera } = useThree();
   const [ref, api] = useSphere(() => ({
     mass: 1,
@@ -11,8 +15,12 @@ export const Player: React.FC = () => {
     position: [0, 1, 0],
   }));
 
-  const pos = useRef([0, 0, 0]);
+  const vel = useRef([0, 0, 0]);
+  useEffect(() => {
+    api.velocity.subscribe((p) => (vel.current = p));
+  }, [api.velocity]);
 
+  const pos = useRef([0, 0, 0]);
   useEffect(() => {
     api.position.subscribe((p) => (pos.current = p));
   }, [api.position]);
@@ -21,6 +29,32 @@ export const Player: React.FC = () => {
     camera.position.copy(
       new Vector3(pos.current[0], pos.current[1], pos.current[2])
     );
+
+    const direction = new Vector3();
+
+    const frontVector = new Vector3(
+      0,
+      0,
+      (moveBackward ? 1 : 0) - (moveForward ? 1 : 0)
+    );
+
+    const sideVector = new Vector3(
+      (moveLeft ? 1 : 0) - (moveRight ? 1 : 0),
+      0,
+      0
+    );
+
+    direction
+      .subVectors(frontVector, sideVector)
+      .normalize()
+      .multiplyScalar(10)
+      .applyEuler(camera.rotation);
+
+    api.velocity.set(direction.x, vel.current[1], direction.z);
+
+    if (jump && Math.abs(vel.current[1] < 0.05)) {
+      api.velocity.set(0, 4, 0);
+    }
   });
 
   return <mesh ref={ref}></mesh>;
---
+++ src/hooks/useKeyboard.tsx
@@ -0,0 +1,58 @@
+import { useCallback, useEffect, useState } from "react";
+
+function actionByKey(key: string): string {
+  const keyActionMap = {
+    KeyW: "moveForward",
+    KeyS: "moveBackward",
+    KeyA: "moveLeft",
+    KeyD: "moveRight",
+    Space: "jump",
+  };
+  return keyActionMap[key];
+}
+
+export const useKeyboard = () => {
+  const [actions, setActions] = useState({
+    moveForward: false,
+    moveBackward: false,
+    moveLeft: false,
+    moveRight: false,
+    jump: false,
+    dirt: false,
+    grass: false,
+    glass: false,
+    wood: false,
+    log: false,
+  });
+
+  const handleKeyDown = useCallback((e) => {
+    const action = actionByKey(e.code);
+    if (action) {
+      setActions((oldState) => ({
+        ...oldState,
+        [action]: true,
+      }));
+    }
+  }, []);
+
+  const handleKeyUp = useCallback((e) => {
+    const action = actionByKey(e.code);
+    if (action) {
+      setActions((oldState) => ({
+        ...oldState,
+        [action]: false,
+      }));
+    }
+  }, []);
+
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeyDown);
+    document.addEventListener("keyup", handleKeyUp);
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+      document.removeEventListener("keyup", handleKeyUp);
+    };
+  }, [handleKeyDown, handleKeyUp]);
+
+  return actions;
+};
